0126216f83ae386133998a47ce777584f2849d8a
Oli Harvey <oliharvey@hotmail.com>
2021-05-01 10:12:02 +0100

    eda underway

---
+++ functions/process_data.py
@@ -0,0 +1,25 @@
+import pandas
+import os 
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+
+
+
+funcs = os.listdir('functions')
+for func in funcs:
+    if func.startswith('__') or func == 'import_all_functions.py':
+        continue
+    func = func.replace('.py','')
+    exec(f'from functions.{func} import *')
+
+def process_data(df: pd.DataFrame) -> pd.DataFrame:
+    dummy_cols = []
+    impute_cols = ['LotFrontage']
+    
+    processed_df = df.copy()
+
+
+    imp = IterativeImputer(max_iter=10, random_state=0)
+    processed_df
+    
+    return processed_df
